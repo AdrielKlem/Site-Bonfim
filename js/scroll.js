983b817a267178f6beee310e57863718b9c85451
Adriel Klem <adrielklem16@gmail.com>
2022-09-23 15:22:23 -0300

    setting in scroll function

--- js/scroll.js
+++ js/scroll.js
@@ -33,7 +33,7 @@ function smoothScrollTo(endX, endY, duration) {
 
     const easeInOutQuart = (time, from, distance, duration) => {
         if ((time /= duration / 2) < 1) {
-            return (distance / 2) * time * time * time * tiem + from
+            return (distance / 2) * time * time * time * time + from
         } else {
             return (-distance / 2) * ((time -= 2) * time * time * time -2) + from
         }
